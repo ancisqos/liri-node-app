4432aa9e25c6eb5e2dd3d7367f25b7e24c98c527
Francis <ancis.arcede@gmail.com>
2017-06-13 13:54:45 -0400

    got twitter to work

--- keys.js
+++ keys.js
@@ -1,9 +1,11 @@
-JavaScript
+// JavaScript
 console.log('this is loaded');
 
-exports.twitterKeys = {
-  consumer_key: DgjabQHTdDl78LdDQuPooJW78,
-  consumer_secret: VcNqdSVh9ZiVZ7eEv4RM9v7WNuu2qeTOhGEkrs6op7Kksm9mUG,
-  access_token_key: 873929168446160901-Yfjy1QpzF9bIxHInLZAOK6t9McfCVYO,
-  access_token_secret: 	HHynhGV97tOz7ruReq2tBGXI3LweF37gxCqhZMm0O0xmK,
+twitterKeys = {
+  consumer_key: 'DgjabQHTdDl78LdDQuPooJW78',
+  consumer_secret: 'VcNqdSVh9ZiVZ7eEv4RM9v7WNuu2qeTOhGEkrs6op7Kksm9mUG',
+  access_token_key: '873929168446160901-Yfjy1QpzF9bIxHInLZAOK6t9McfCVYO',
+  access_token_secret: 	'HHynhGV97tOz7ruReq2tBGXI3LweF37gxCqhZMm0O0xmK',
 }
+
+module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,48 +1,61 @@
+// TESTING FILE
 console.log("LIRI Bot is ready to go.");
 
-const fs = require('fs');
+// REQUIRING PROJECT DEPENDENCIES
+var fs = require('fs');
 var Twitter = require('twitter');
-var spotify = require('spotify');
-const keys = require('./keys.js');
-var twit = new Twitter(keys);
-var command = process.agrv[2]
-var parameters = {
-	'screen_name': 'hajibdajaj',
-	'count': 20
-}
+var keys = require('./keys.js');
+var omdb = require('omdb');
 
-//code for Twitter 'my-tweets' Command
-if (command === 'my-tweets') {
-	twit.get('statuses/user_timeline', parameters, gotData);
-	function gotData(error, data, response){
-		var tweets = data;
-		for (var i = 0; i < tweets.length; i++){
-			console.log(tweets[i].text);
-			console.log(tweets[i].created_at);
-		}
-	}
-}
+// TESTING TO MAKE SURE KEYS IS IMPORTED SUCCESSFULLY
+console.log(keys)
 
-//code for Spotify 'spotify this song' Command
-else if (command === 'spotify-this-song') {
- 
-	spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
- 
-    // Do something with 'data' 
+// TESTING COMMAND
+var command = process.argv[2]
+
+// CREATING CLIENT FROM APPLICATION API KEYS
+var client = new Twitter({
+    consumer_key: keys.consumer_key,
+    consumer_secret: keys.consumer_secret,
+    access_token_key: keys.access_token_key,
+    access_token_secret: keys.access_token_secret
 });
-	console.log()
+
+// PARAMETERS USED TO ACCESS TWITTER ACCOUNT INFO
+var parameters = {
+    'screen_name': 'hajibdajaj',
+    'count': 20
 }
 
-//code for Movie 'movie-this' Command
-else if (command === 'movie-this') {
-	console.log()
-}
+// CONDITIONAL STATEMENT TO RUN TWITTER NPM PACKAGE
+if (command === 'my-tweets') {
+    console.log('Tweetz comin yo way');
+    client.get('statuses/user_timeline', parameters, gotData);
 
-//code for 'do-what-it-says' Command
-else if (command === 'do-what-it-says') {
-	console.log()
-}
+    function gotData(error, data, response) {
+        var tweets = data;
+        for (var i = 0; i < tweets.length; i++) {
+            console.log(tweets[i].text);
+            console.log(tweets[i].created_at);
+        }
+    }
+};
+
+// CONDITIONAL STATEMENT TO RUN OMDB NPM PACKAGE
+if (command === 'movie-this') {
+    console.log('Netflix and chill?');
+    var movieTitle = process.argv[3];
+    request("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body){
+
+    	
+
+    	
+// //code for Movie 'movie-this' Command
+// else if (command === 'movie-this') {
+// 	console.log()
+// }
+
+// //code for 'do-what-it-says' Command
+// else if (command === 'do-what-it-says') {
+// 	console.log()
+// }
